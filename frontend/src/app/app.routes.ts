9ac8c4a868dd92931084deb1ee32ecb55c976ea5
Jean Souza <jeansouza199487@gmail.com>
2026-07-01 12:36:51 -0300

    feat(frontend): configura sistema de rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,63 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/auth-layout/auth-layout.component')
+        .then(m => m.AuthLayoutComponent),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./features/auth/login/login.component')
+            .then(m => m.LoginComponent)
+      },
+      {
+        path: 'cadastro',
+        loadComponent: () =>
+          import('./features/auth/cadastro/cadastro.component')
+            .then(m => m.CadastroComponent)
+      }
+    ]
+  },
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/main-layout/main-layout.component')
+        .then(m => m.MainLayoutComponent),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/dashboard/dashboard.component')
+            .then(m => m.DashboardComponent)
+      },
+      {
+        path: 'transferencia',
+        loadComponent: () =>
+          import('./features/transferencia/transferencia.component')
+            .then(m => m.TransferenciaComponent)
+      },
+      {
+        path: 'extrato',
+        loadComponent: () =>
+          import('./features/extrato/extrato.component')
+            .then(m => m.ExtratoComponent)
+      },
+      {
+        path: 'perfil',
+        loadComponent: () =>
+          import('./features/perfil/perfil.component')
+            .then(m => m.PerfilComponent)
+      }
+    ]
+  }
+];
