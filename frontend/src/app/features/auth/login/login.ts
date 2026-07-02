7e4c4bfbb666a275f3cb7e395d2be4dbd890ef48
Jean Souza <jeansouza199487@gmail.com>
2026-07-02 11:44:14 -0300

    feat(auth): implementa tela de login

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    email: ['', [
+      Validators.required,
+      Validators.email
+    ]],
+
+    senha: ['', [
+      Validators.required,
+      Validators.minLength(8)
+    ]]
+  });
+
+  onSubmit() {
+  console.log(this.loginForm.value);
+  }
+}
