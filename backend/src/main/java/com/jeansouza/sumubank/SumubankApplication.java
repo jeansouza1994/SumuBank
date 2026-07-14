367b1f332df6c5d097c6d247c4ed2f1d3e6eb3a0
Jean Souza <jeansouza199487@gmail.com>
2026-07-14 15:24:01 -0300

    chore(backend): inicializa projeto spring boot

---
+++ backend/src/main/java/com/jeansouza/sumubank/SumubankApplication.java
@@ -0,0 +1,13 @@
+package com.jeansouza.sumubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumubankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeansouza/sumubank/SumubankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeansouza.sumubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
