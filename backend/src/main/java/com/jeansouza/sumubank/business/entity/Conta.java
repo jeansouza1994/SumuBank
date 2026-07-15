e24587425b335c05a20add922919cab0a9638059
Jean Souza <jeansouza199487@gmail.com>
2026-07-15 12:21:06 -0300

    feat(backend): Implementa entidades iniciais de usuário e conta

---
+++ backend/src/main/java/com/jeansouza/sumubank/business/entity/Conta.java
@@ -0,0 +1,22 @@
+package com.jeansouza.sumubank.business.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "contas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id", nullable = false, unique = true)
+    private Usuario usuario;
+}
---
+++ backend/src/main/java/com/jeansouza/sumubank/business/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.jeansouza.sumubank.business.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @OneToOne(mappedBy = "usuario")
+    private Conta conta;
+
+}
